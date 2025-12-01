145a48f800ec15c83ba210297fdba0b87d459def
BTman01 <74837483+BTman01@users.noreply.github.com>
2025-11-30 23:01:06 -0500

    Delete components directory

--- components/InstallationWizard.tsx
+++
@@ -1,6 +0,0 @@
-
-// This component is no longer used. The installation flow is now
-// triggered by the "Create New Profile" button on the dashboard,
-// which directly shows the InstallProgress component.
-// This file can be safely deleted.
-export {};
